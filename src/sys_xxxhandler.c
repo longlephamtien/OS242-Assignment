fb40517a65d1dca3b8e851f24153c1c3a401f213
thanhtan26 <tan.dothanh2506@hcmut.edu.vn>
2025-04-04 17:43:29 +0700

    resolve

--- src/sys_xxxhandler.c
+++ src/sys_xxxhandler.c
@@ -2,10 +2,3 @@
 #include <syscall.h>
 #include <stdlib.h>
 
-int __sys_xxx_handler(struct pcb_t *caller, struct sc_regs* reg)
-{
-    /* data */
-    printf("The first system call parameter %d\n", reg->a1);
-    return 0;
-};
-
