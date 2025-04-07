f7e5c3a9896113c93bfa8b47c6410efcc8bf4c3f
thanhtan26 <tan.dothanh2506@hcmut.edu.vn>
2025-04-07 12:03:40 +0700

    new system call for IPC

--- src/sys_xxxhandler.c
+++ src/sys_xxxhandler.c
@@ -2,3 +2,10 @@
 #include <syscall.h>
 #include <stdlib.h>
 
+int __sys_xxxhandler(struct pcb_t *caller, struct sc_regs* reg)
+{
+    
+    printf("The first system call parameter %d\n", reg->a1);
+    return 0;
+};
+
