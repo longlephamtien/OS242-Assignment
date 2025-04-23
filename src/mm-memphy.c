35efd05b6e267b1ff410b7489f15b657c37f80e4
longlephamtien <lephamtienlong.4754@gmail.com>
2025-04-23 12:58:47 +0700

    chore: merge and test all

--- src/mm-memphy.c
+++ src/mm-memphy.c
@@ -160,9 +160,17 @@ int MEMPHY_get_freefp(struct memphy_struct *mp, int *retfpn)
 
 int MEMPHY_dump(struct memphy_struct *mp)
 {
-  /*TODO dump memphy contnt mp->storage
-   *     for tracing the memory content
-   */
+   /*TODO dump memphy contnt mp->storage
+    *     for tracing the memory content
+    */
+   printf("===== PHYSICAL MEMORY DUMP =====\n");
+   for (int i = 0; i < mp->maxsz; i++)
+   {
+      if (mp->storage[i] != 0)
+         printf("Byte %08x: %d\n", i, mp->storage[i]);
+   }
+   printf("===== PHYSICAL MEMORY END-DUMP =====\n");
+   printf("================================================================\n");
    return 0;
 }
 
--- src/sys_killall.c
+++ src/sys_killall.c
@@ -12,23 +12,51 @@
 #include "syscall.h"
 #include "stdio.h"
 #include "libmem.h"
+#include "queue.h"
+#include "mm.h"
+#include <string.h>
+#include <stdlib.h>
 
-int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
+void match_terminate(struct pcb_t *caller, struct queue_t *queue, const char *proc_name)
+{
+    for (int i = 0; i < queue->size;)
+    {
+        if (queue->proc[i] && strcmp(queue->proc[i]->path, proc_name) == 0)
+        {
+            struct pcb_t *pcb = queue->proc[i];
+            for (int j = i; j < queue->size - 1; j++)
+                queue->proc[j] = queue->proc[j + 1];
+            queue->size--;
+            free(pcb);
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
+
+int __sys_killall(struct pcb_t *caller, struct sc_regs *regs)
 {
     char proc_name[100];
     uint32_t data;
 
-    //hardcode for demo only
+    // hardcode for demo only
     uint32_t memrg = regs->a1;
-    
+
     /* TODO: Get name of the target proc */
-    //proc_name = libread..
+    // proc_name = libread..
     int i = 0;
     data = 0;
-    while(data != -1){
+    while (data != -1)
+    {
         libread(caller, memrg, i, &data);
-        proc_name[i]= data;
-        if(data == -1) proc_name[i]='\0';
+        proc_name[i] = data;
+        if (data == -1)
+        {
+            proc_name[i] = '\0';
+            break;
+        }
         i++;
     }
     printf("The procname retrieved from memregionid %d is \"%s\"\n", memrg, proc_name);
@@ -36,13 +64,19 @@ int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
     /* TODO: Traverse proclist to terminate the proc
      *       stcmp to check the process match proc_name
      */
-    //caller->running_list
-    //caller->mlq_ready_queu
+    // caller->running_list
+    // caller->mlq_ready_queu
 
-    /* TODO Maching and terminating 
+    /* TODO Maching and terminating
      *       all processes with given
      *        name in var proc_name
      */
 
-    return 0; 
+    match_terminate(caller, caller->running_list, proc_name);
+    match_terminate(caller, caller->mlq_ready_queue, proc_name);
+    match_terminate(caller, caller->ready_queue, proc_name);
+
+    printf("All processes with name \"%s\" have been terminated.\n", proc_name);
+
+    return 0;
 }
