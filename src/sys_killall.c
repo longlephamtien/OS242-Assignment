8eb7a7bad94e9c7c07ce64300c87c76c97ac24fa
thanhtan26 <tan.dothanh2506@hcmut.edu.vn>
2025-04-26 06:03:50 +0700

    fix:kill processes by name in running list queue

--- src/sys_killall.c
+++ src/sys_killall.c
@@ -12,6 +12,8 @@
 #include "syscall.h"
 #include "stdio.h"
 #include "libmem.h"
+#include "queue.h"
+#include <string.h>
 
 int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
 {
@@ -24,7 +26,7 @@ int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
     /* TODO: Get name of the target proc */
     //proc_name = libread..
     int i = 0;
-    data = 0;
+    data = 0;   
     while(data != -1){
         libread(caller, memrg, i, &data);
         proc_name[i]= data;
@@ -39,10 +41,41 @@ int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
     //caller->running_list
     //caller->mlq_ready_queu
 
+    if (caller->running_list == NULL || caller->running_list->size == 0) {
+        return -1;
+    }
+
+    struct queue_t *q = caller->running_list;
+    if (!empty(q)) {
+        for (int i = 0; i < q->size; i++) {
+            char proc_name_in_queue[100];
+            int j = 0;
+            uint32_t temp = 0;
+
+            while (temp != -1) {
+                libread(q->proc[i], q->proc[i]->regs[0], j, temp);
+                if (temp == -1) {
+                    proc_name_in_queue[j] = '\0';
+                    break;
+                }
+                proc_name_in_queue[j] = temp;
+                j++;
+            }
+
     /* TODO Maching and terminating 
      *       all processes with given
      *        name in var proc_name
      */
 
-    return 0; 
+            if (strcmp(proc_name, proc_name_in_queue) == 0) {
+                struct pcb_t *proc = q->proc[i];
+                for (int t = i; t < q->size - 1; t++) {
+                    q->proc[t] = q->proc[t + 1];
+                }
+                libfree(proc, regs[0]);
+            }
+        }
+    }
+
+    return -1; 
 }
